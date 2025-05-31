a835e7da309c7149af4da7ba737ba356006f9417
Aldevieve Laplap <aldelaplap@gmail.com>
2025-05-31 17:55:58 +0800

    Update admin.py

--- myfolder/nutritional_root/accounts/admin.py
+++ myfolder/nutritional_root/accounts/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from .models import Page
+from .models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id_no','last','first','middle', 'age', 'birthday', 'weight', 'height', 'gender']
+
+admin.site.register(Page)
+admin.site.register(Student,StudentAdmin)
 
